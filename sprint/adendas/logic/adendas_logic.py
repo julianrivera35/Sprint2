5ebab40de93862add9f81f3a9b7c4b0473822413
g-carvajal <g.carvajal@uniandes.edu.co>
2023-03-18 02:48:27 -0500

    Algo?

--- sprint/adendas/logic/adendas_logic.py
+++ sprint/adendas/logic/adendas_logic.py
@@ -17,4 +17,43 @@ def update_adenda(adenda_pk, new_adenda):
     return adenda
 
 def create_adenda(me):
-    adenda = Adenda(name)
+    adenda = Adenda(name = me["name"], descripcion = ["descripcion"], adenda=["adenda"])
+    adenda.save()
+    return adenda
+
+def adendas_view(request):
+    if request.method == 'GET':
+        adendas = me.get_adendas()
+        adendas_dto = serializers.serialize('json',  adendas)
+        return HttpResponse(adendas_dto, 'application/json')
+    
+    
+@csrf_exempt
+def adendas_view(request, pk):
+    if request.method == 'GET':
+        adenda_dto = me.get_adenda(pk)
+        adenda = serializers.serialize('json',[adenda_dto,])
+        return HttpResponse(adenda, 'appliaction/json')
+    
+    if request.method == 'PUT':
+        adenda_dto = me.update_adenda(pk, json.loads(request.boy))
+        adenda = serializers.serialize('json',[adenda_dto,])
+        return HttpResponse(adenda, 'application/json')
+    
+@csrf_exempt
+def adenda_views(request):
+    if request.method == 'GET':
+        id = request.GET.get("id", None)
+        if id:
+            adenda_dto = me.get_adenda(id)
+            adenda = serializers.serialize('json',[adenda_dto,])
+            return HttpResponse(adenda, 'apllication/json')
+        else:
+            adendas_dto = me.create_adenda(json.loads(request.body))
+            adenda = serializers.serialize('json', adendas_dto)
+            return HttpResponse(adenda, 'application/json')
+        
+    if request.method == 'POST':
+        adenda_dto = me.create_adenda(json.loads(request.body))
+        adenda = serializers.serialize('json', [adenda_dto,])
+        return HttpResponse(adenda, 'application/json')
