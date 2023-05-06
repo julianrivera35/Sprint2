e51f15339172e72ebe63d4a6dc2168b10769740c
julianrivera35 <jc.riveram1@uniandes.edu.co>
2023-05-05 20:07:09 -0500

    las weas de seguridad estas

--- sprint/historias/views.py
+++ sprint/historias/views.py
@@ -3,24 +3,30 @@ from django.http import HttpResponse
 from django.core import serializers
 import json
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from monitoring.auth0backend import getRole
 
-@csrf_exempt
+@login_required
 def historias_view(request):
-    if request.method == 'GET':
-        id = request.GET.get("id", None)
-        if id:
-            historia_dto = me.get_historia(id)
+    role = getRole(request)
+    if role == "Medico":
+        if request.method == 'GET':
+            id = request.GET.get("id", None)
+            if id:
+                historia_dto = me.get_historia(id)
+                historia = serializers.serialize('json', [historia_dto,])
+                return HttpResponse(historia, 'application/json')
+            else:
+                historias_dto = me.get_historias()
+                historias = serializers.serialize('json', historias_dto)
+                return HttpResponse(historias, 'application/json')
+
+        if request.method == 'POST':
+            historia_dto = me.create_historia(json.loads(request.body))
             historia = serializers.serialize('json', [historia_dto,])
             return HttpResponse(historia, 'application/json')
-        else:
-            historias_dto = me.get_historias()
-            historias = serializers.serialize('json', historias_dto)
-            return HttpResponse(historias, 'application/json')
-
-    if request.method == 'POST':
-        historia_dto = me.create_historia(json.loads(request.body))
-        historia = serializers.serialize('json', [historia_dto,])
-        return HttpResponse(historia, 'application/json')
+    else:
+        return HttpResponse("Unauthorized User")
     
     #if request.method == 'DELETE':
     #    pk = request.GET.get("id", None)
@@ -28,17 +34,21 @@ def historias_view(request):
     #    historia = serializers.serialize('json',[historia_dto,])
     #    return HttpResponse('application/json')
 
-@csrf_exempt
+@login_required
 def historia_view(request, pk):
-    if request.method == 'GET':
-        historia_dto = me.get_historia(pk)
-        historia = serializers.serialize('json', [historia_dto,])
-        return HttpResponse(historia, 'application/json')
+    role = getRole(request)
+    if role == "Medico":
+        if request.method == 'GET':
+            historia_dto = me.get_historia(pk)
+            historia = serializers.serialize('json', [historia_dto,])
+            return HttpResponse(historia, 'application/json')
 
-    if request.method == 'PUT':
-        historia_dto = me.update_historia(pk, json.loads(request.body))
-        historia = serializers.serialize('json', [historia_dto,])
-        return HttpResponse(historia, 'application/json')
+        if request.method == 'PUT':
+            historia_dto = me.update_historia(pk, json.loads(request.body))
+            historia = serializers.serialize('json', [historia_dto,])
+            return HttpResponse(historia, 'application/json')
+    else:
+        return HttpResponse("Unauthorized User")
     
     #if request.method == 'DELETE':
     #    historia_dto = me.historia_delete(pk)
