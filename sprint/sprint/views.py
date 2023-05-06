16b43176bbefffc357bd44df6789f2bb03774cc7
julianrivera35 <jc.riveram1@uniandes.edu.co>
2023-05-05 23:31:52 -0500

    Vuelta

--- sprint/sprint/views.py
+++ sprint/sprint/views.py
@@ -5,12 +5,17 @@ def home(request):
 
 def contenidoHTML(request):
     contenido = """ 
-
-    <div style=" display: flex; align-items: center; justify-content: center;" class="login-box auth0-box before">
-            <img style="width:80%; " src="/static/media/monitor.png" />
-    </div>
-
-    
+    <html>
+    <head>
+    <style type= "text/css"> div{background-color:3BEED5; width: 100%; height: 100px; font-weight:bold; padding: 5px;}
+    </style>
+    </head>
+    <body>
+    <div><center><font size=30>Widmy</font></center></div>
+    <h1> <font size=15>Como eres un paciente en realidad no tienes acceso a nada por acá </font></h1>
+    <img src="https://i.pinimg.com/550x/62/21/4e/62214ea9432fef2ca9974ef274d0924b.jpg" width="500" height="500">
+    </body>
+    </html>
     
     
     """
