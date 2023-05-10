3e48a48ac22ea59ae0f262ce92fd8a951e70092e
julianrivera35 <jc.riveram1@uniandes.edu.co>
2023-05-10 16:57:52 -0500

    Cambios en el html

--- sprint/historias/views.py
+++ sprint/historias/views.py
@@ -4,29 +4,27 @@ from django.core import serializers
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
-from sprint.auth0backend import getRole
 
 @login_required
 def historias_view(request):
-    role = getRole(request)
-    if role == "Medico":
-        if request.method == 'GET':
-            id = request.GET.get("id", None)
-            if id:
-                historia_dto = me.get_historia(id)
-                historia = serializers.serialize('json', [historia_dto,])
-                return HttpResponse(historia, 'application/json')
-            else:
-                historias_dto = me.get_historias()
-                historias = serializers.serialize('json', historias_dto)
-                return HttpResponse(historias, 'application/json')
-
-        if request.method == 'POST':
-            historia_dto = me.create_historia(json.loads(request.body))
+    
+    
+    if request.method == 'GET':
+        id = request.GET.get("id", None)
+        if id:
+            historia_dto = me.get_historia(id)
             historia = serializers.serialize('json', [historia_dto,])
             return HttpResponse(historia, 'application/json')
-    else:
-        return HttpResponse("Unauthorized User")
+        else:
+            historias_dto = me.get_historias()
+            historias = serializers.serialize('json', historias_dto)
+            return HttpResponse(historias, 'application/json')
+
+    if request.method == 'POST':
+        historia_dto = me.create_historia(json.loads(request.body))
+        historia = serializers.serialize('json', [historia_dto,])
+        return HttpResponse(historia, 'application/json')
+
     
     #if request.method == 'DELETE':
     #    pk = request.GET.get("id", None)
@@ -36,19 +34,18 @@ def historias_view(request):
 
 @login_required
 def historia_view(request, pk):
-    role = getRole(request)
-    if role == "Medico":
-        if request.method == 'GET':
-            historia_dto = me.get_historia(pk)
-            historia = serializers.serialize('json', [historia_dto,])
-            return HttpResponse(historia, 'application/json')
+    
+    
+    if request.method == 'GET':
+        historia_dto = me.get_historia(pk)
+        historia = serializers.serialize('json', [historia_dto,])
+        return HttpResponse(historia, 'application/json')
 
-        if request.method == 'PUT':
-            historia_dto = me.update_historia(pk, json.loads(request.body))
-            historia = serializers.serialize('json', [historia_dto,])
-            return HttpResponse(historia, 'application/json')
-    else:
-        return HttpResponse("Unauthorized User")
+    if request.method == 'PUT':
+        historia_dto = me.update_historia(pk, json.loads(request.body))
+        historia = serializers.serialize('json', [historia_dto,])
+        return HttpResponse(historia, 'application/json')
+
     
     #if request.method == 'DELETE':
     #    historia_dto = me.historia_delete(pk)
--- sprint/sprint/views.py
+++ sprint/sprint/views.py
@@ -3,20 +3,116 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse("Esta es la página inicial de Widmy!!")
 
-def contenidoHTML(request):
-    contenido = """ 
+def HTMLSeleccionDeUsuario(request):
+    contenido = """
+    
+    <!DOCTYPE html>
     <html>
     <head>
-    <style type= "text/css"> div{background-color:3BEED5; width: 100%; height: 100px; font-weight:bold; padding: 5px;}
+    <title>Iniciar Sesión</title>
+    <style>
+        body {
+        margin: 0;
+        padding: 0;
+        font-family: Arial, sans-serif;
+        }
+        .container {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+        text-align: center;
+        }
+        h1 {
+        margin-top: 0;
+        }
+        .login-button {
+        background-color: #4CAF50;
+        color: white;
+        border: none;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 10px;
+        cursor: pointer;
+        }
     </style>
     </head>
     <body>
-    <div><center><font size=30>Widmy</font></center></div>
-    <h1> <font size=15>Como eres un paciente en realidad no tienes acceso a nada por acá </font></h1>
-    <img src="https://i.pinimg.com/550x/62/21/4e/62214ea9432fef2ca9974ef274d0924b.jpg" width="500" height="500">
+    <div class="container">
+        <h1>Iniciar Sesión</h1>
+        <p>Selecciona el tipo de cuenta con la que deseas iniciar sesión:</p>
+        <button class="login-button" onclick="window.location.href='login-medico.html'">Médico</button>
+        <button class="login-button" onclick="window.location.href='http://127.0.0.1:8000/admin'">Administrador</button>
+    </div>
     </body>
     </html>
     
     
+    """
+    
+    
+    
+    return HttpResponse(contenido)
+
+def contenidoHTML(request):
+    contenido = """ 
+    <!DOCTYPE html>
+<html>
+<head>
+  <title>Página de inicio - Hospital</title>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      font-family: Arial, sans-serif;
+    }
+    .header {
+      background-color: #f2f2f2;
+      padding: 20px;
+      text-align: right;
+    }
+    .login-button {
+      background-color: #4CAF50;
+      color: white;
+      border: none;
+      padding: 10px 20px;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      font-size: 16px;
+      margin-right: 20px;
+    }
+    .content {
+      padding: 20px;
+    }
+    .doctor-images {
+      display: flex;
+      justify-content: center;
+      margin-top: 30px;
+    }
+    .doctor-images img {
+      width: 200px;
+      margin: 10px;
+    }
+  </style>
+</head>
+<body>
+  <div class="header">
+    <a class="login-button" href="http://127.0.0.1:8000/inicio/">Iniciar sesión</a>
+  </div>
+  <div class="content">
+    <h1>Bienvenido a Widmy</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+    <div class="doctor-images">
+      <img src="https://img.freepik.com/vector-gratis/ilustracion-concepto-doctores_114360-1515.jpg?w=1380&t=st=1683749448~exp=1683750048~hmac=f8dbfb317013900b993eb388eef7f8b97a97be18fd704558a80a9350c54835d5" alt="Doctor 1">
+      <img src="https://img.freepik.com/vector-gratis/ilustracion-concepto-doctores_114360-1515.jpg?w=1380&t=st=1683749448~exp=1683750048~hmac=f8dbfb317013900b993eb388eef7f8b97a97be18fd704558a80a9350c54835d5" alt="Doctor 2">
+      <img src="https://img.freepik.com/vector-gratis/ilustracion-concepto-doctores_114360-1515.jpg?w=1380&t=st=1683749448~exp=1683750048~hmac=f8dbfb317013900b993eb388eef7f8b97a97be18fd704558a80a9350c54835d5" alt="Doctor 3">
+    </div>
+  </div>
+</body>
+</html>
+        
     """
     return HttpResponse(contenido)
