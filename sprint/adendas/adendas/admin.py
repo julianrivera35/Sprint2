3618eb526ccfe70744e6296929e86db97bc15f0a
julianrivera35 <jc.riveram1@uniandes.edu.co>
2023-06-02 19:43:51 -0500

    Sonar

--- sprint/adendas/adendas/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Adenda
-
-admin.site.register(Adenda)
-# Register your models here.
-
--- sprint/adendas/adendas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdendasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'adendas'
